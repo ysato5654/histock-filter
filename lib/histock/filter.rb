20ed542577a3f378a04094da4287e1bb2b5475dd
yuya sato <ysato.5654@gmail.com>
2020-05-09 23:29:38 +0800

    support 'monthly_revenue', 'income_statement', 'profit_ratio' and 'income_rate' request - need version is ~> 0.4.0

--- lib/histock/filter.rb
+++ lib/histock/filter.rb
@@ -3,15 +3,27 @@ require File.expand_path(File.dirname(__FILE__)) + '/filter/version'
 
 module Histock
     class Filter
-        CONFIG = ['dividend_policy', 'financial_statements']
+        CONFIG = [
+            ['monthly_revenue', 'income_statement', 'dividend_policy', 'profit_ratio'], # one argument
+            ['income_rate']                                                             # two arguments
+        ]
 
         def initialize
         end
 
-        CONFIG.each do |method_name|
+        CONFIG.first.each do |method_name|
             define_method method_name do |param|
                 histock = Histock::Simplefilter.new
                 table = histock.send(method_name, param)
+
+                table[1..-1].map { |e| table.first.zip(e).to_h }
+            end
+        end
+
+        CONFIG.last.each do |method_name|
+            define_method method_name do |param, param2|
+                histock = Histock::Simplefilter.new
+                table = histock.send(method_name, param, param2)
 
                 table[1..-1].map { |e| table.first.zip(e).to_h }
             end
