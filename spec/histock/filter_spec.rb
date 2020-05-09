f69d3614751f49bdd21e48c70d6c0c82a4893a71
yuya sato <ysato.5654@gmail.com>
2020-05-09 23:31:18 +0800

    add test code for 'monthly_revenue', 'income_statement', 'profit_ratio' and 'income_rate' method

--- spec/histock/filter_spec.rb
+++ spec/histock/filter_spec.rb
@@ -3,12 +3,46 @@ RSpec.describe Histock::Filter do
         expect(Histock::Filter::VERSION).not_to be nil
     end
 
-        let :histock do
+    let :histock do
         Histock::Filter.new
     end
 
     let :code do
         '2330'
+    end
+
+    describe '#monthly_revenue(code)' do
+        subject do
+            histock.monthly_revenue(code)
+        end
+
+        let :keys do
+            ["年度/月份", "單月營收", "去年同月營收", "單月月增率", "單月年增率", "累計營收", "去年累計營收", "累積年增率"]
+        end
+
+        describe 'key' do
+            it 'is correct' do
+                expect(subject.first.keys).to eq(keys)
+                expect(subject.last.keys).to eq(keys)
+            end
+        end
+    end
+
+    describe '#income_statement(code)' do
+        subject do
+            histock.income_statement(code)
+        end
+
+        let :keys do
+            ["年度/季別", "營收", "毛利", "營業利益", "稅前淨利", "稅後淨利"]
+        end
+
+        describe 'key' do
+            it 'is correct' do
+                expect(subject.first.keys).to eq(keys)
+                expect(subject.last.keys).to eq(keys)
+            end
+        end
     end
 
     describe '#dividend_policy(code)' do
@@ -28,19 +62,76 @@ RSpec.describe Histock::Filter do
         end
     end
 
-    describe '#financial_statements(code)' do
+    describe '#profit_ratio(code)' do
         subject do
-            histock.financial_statements(code)
+            histock.profit_ratio(code)
         end
 
         let :keys do
-            ["年度/月份", "單月營收", "去年同月營收", "單月月增率", "單月年增率", "累計營收", "去年累計營收", "累積年增率"]
+            ["年度/季別", "毛利率", "營業利益率", "稅前淨利率", "稅後淨利率"]
         end
 
         describe 'key' do
             it 'is correct' do
                 expect(subject.first.keys).to eq(keys)
                 expect(subject.last.keys).to eq(keys)
+            end
+        end
+    end
+
+    describe '#income_rate(code, term)' do
+        subject do
+            histock.income_rate(code, term)
+        end
+
+        describe 'term is month' do
+            let :keys do
+                ["年度/季別", "ROE", "ROA"]
+            end
+
+            let :term do
+                'month'
+            end
+
+            describe 'key' do
+                it 'is correct' do
+                    expect(subject.first.keys).to eq(keys)
+                    expect(subject.last.keys).to eq(keys)
+                end
+            end
+        end
+
+        describe 'term is quarter' do
+            let :keys do
+                ["年度/季別", "近四季ROE", "近四季ROA"]
+            end
+
+            let :term do
+                'quarter'
+            end
+
+            describe 'key' do
+                it 'is correct' do
+                    expect(subject.first.keys).to eq(keys)
+                    expect(subject.last.keys).to eq(keys)
+                end
+            end
+        end
+
+        describe 'term is year' do
+            let :keys do
+                ["年度", "年度ROE", "年度ROA"]
+            end
+
+            let :term do
+                'year'
+            end
+
+            describe 'key' do
+                it 'is correct' do
+                    expect(subject.first.keys).to eq(keys)
+                    expect(subject.last.keys).to eq(keys)
+                end
             end
         end
     end
